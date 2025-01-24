0eaf4ed5aeb88e79248b25bc6c6778c035331534
alexisvalory <valoryalexis@gmail.com>
2025-01-24 17:37:18 +0200

    update index FR & EN
    update index.html and index_en.html

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@
 const filterButtons = document.querySelectorAll("#filter-buttons .btn-sm");
 const filterableCards = document.querySelectorAll("#filterable-cards .portfolio-item");
 
-
-let activeFilter = "all"
-
 // Function to filter cards based on filter buttons
 const filterCards = (e) => {
-
-    const clickedFilter = e.target.dataset.filter;
-    if(clickedFilter === activeFilter) return;
-
     document.querySelector("#filter-buttons .active").classList.remove("active");
     e.target.classList.add("active");
 
@@ -19,8 +12,7 @@ const filterCards = (e) => {
         if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
             return card.classList.replace("hide", "show");
         }
-        if(!classList.contains("hide"))
-            card.classList.add("hide");
+        card.classList.add("hide");
     });
 }
 
